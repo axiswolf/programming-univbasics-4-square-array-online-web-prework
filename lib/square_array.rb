ed190ddbd963f4164d349624c17e70389c62b3e6
Karen V <karenvo@usc.edu>
2019-12-26 12:01:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
   # your code here
   i = 0
-  
+
     while i < numbers.size
       # Fill the result array
       result[i] = numbers[i] ** 2
-  
+
       # and don't forget to increase the index,
       # otherwise the loop will run forever.
       i += 1
